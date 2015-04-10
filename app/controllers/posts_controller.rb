95409cdde965588399dafe01b2a56a532eb7c332
iantheninja <munene.ian@live.com>
2015-04-10 16:31:57 +0300

    working index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,13 @@
 class PostsController < ApplicationController
   def index
+  	@posts = Post.all
   end
 
   def new
   end
 
   def show
+  	@post = Post.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-
-  get 'posts/new'
-
-  get 'posts/show'
-
-  get 'posts/edit'
-
   resources :posts
 end
